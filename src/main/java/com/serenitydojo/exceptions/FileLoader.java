a609487a5f5e312e5ddaaaca6e1234d94f15199b
DESKTOP-2056LD4\snigg <navarone1970@gmail.com>
2021-04-01 16:53:57 +0100

    Lesson 12 Exercises

--- src/main/java/com/serenitydojo/exceptions/FileLoader.java
+++ src/main/java/com/serenitydojo/exceptions/FileLoader.java
@@ -1,19 +1,32 @@
 package com.serenitydojo.exceptions;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
+import java.nio.file.Paths;
 
 public class FileLoader {
     public String readHelloWorld() throws IOException {
-        return "";//Files.readString(Paths.get("src/main/resources/hello.txt"));
+       return Files.readString(Paths.get("src/main/resources/hello.txt")); //"";
     }
 
     public Boolean fileContainsText(String filename, String expectedText) {
-        String path = "src/main/resources/" + filename;
-        return null;// (Files.readString(Paths.get(path)).contains(expectedText));
+        try {
+            String path = "src/main/resources/" + filename;
+            return (Files.readString(Paths.get(path)).contains(expectedText)) ? true : false;
+        } catch (IOException e) {
+            return false;
+        }
     }
 
     public Boolean fileHasText(String filename, String expectedText) {
-        String path = "src/main/resources/" + filename;
-        return null;// (Files.readString(Paths.get(path)).contains(expectedText));
+
+        try {
+            String path = "src/main/resources/" + filename;
+            return (Files.readString(Paths.get(path)).contains(expectedText));
+        } catch (IOException e){
+            throw new MissingWelcomeFileException("No file found for "+filename,e);
+        }
     }
 }
